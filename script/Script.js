38d6b6e62968f924134d08c612cad2ec70eac5f3
naheemtaj44 <naheemtaj44@gmail.com>
2024-11-17 22:31:12 +0500

    .

---
+++ script/Script.js
@@ -0,0 +1,7 @@
+/*=============================== typing animation=================================*/
+var typed =new Typed(".typing",{
+    strings:["devop engineer","web developer","cloud controller"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+})
