08cd8d6d53bcf171ebc43d6729a3c51a77108403
naheemtaj44 <naheemtaj44@gmail.com>
2024-11-26 19:01:04 +0500

    add projrct

--- script/Script.js
+++ script/Script.js
@@ -1,6 +1,6 @@
 /*=============================== typing animation=================================*/
 var typed = new Typed(".typing",{
-    strings:["","devop engineer","web developer","cloud controller"],
+    strings:["","Frontend Developer","Backend Developer",""],
     typeSpeed:100,
     backSpeed:60,
     loop:true,
